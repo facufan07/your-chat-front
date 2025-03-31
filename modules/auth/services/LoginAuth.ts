933af78ec36c217abb04ebd8f55ec8d4d95b1592
Facundo <122570343+facufan07@users.noreply.github.com>
2025-03-31 19:22:16 -0300

    Changed url backend

--- modules/auth/services/LoginAuth.ts
+++ modules/auth/services/LoginAuth.ts
@@ -4,7 +4,9 @@ import { error } from "@/interfaces/interfaces";
 
 export async function loginAuth(mail: string, password: string):Promise<number | error> {
     try {
-        const res = await axios.post<number>("https://your-chat-back-production.up.railway.app/api/v1/auth/login", { mail: mail, password: password }, { withCredentials: true });
+        const res = await axios.post<number>("https://your-chat-back-production.up.railway.app/api/v1/auth/login", 
+            { mail: mail, password: password }, 
+            { withCredentials: true });
         console.log("Token recibido:", res.data);
         return res.data;
     } catch (err: any) {
