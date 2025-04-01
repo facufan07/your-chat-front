964cd90effa0afe094cf438a639d76f1a91879d9
Facundo <122570343+facufan07@users.noreply.github.com>
2025-04-01 13:04:06 -0300

    Problem with pagination fixed

--- utils/getLastPage.ts
+++ utils/getLastPage.ts
@@ -2,8 +2,9 @@ import { totalPages } from "@/interfaces/interfaces";
 import axios from "axios";
 
 export async function getLastPage(chatId: number, path: string) {
+    const url = process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "https://your-chat-back-production.up.railway.app/" : "http://localhost:8080/";
     try{
-        const res = await axios.get<totalPages>(`http://localhost:8080/api/v1/${path}${chatId === -1 ? "" : `/${chatId}`}`,{
+        const res = await axios.get<totalPages>(`${url}api/v1/${path}${chatId === -1 ? "" : `/${chatId}`}`,{
             withCredentials: true,
         });
         console.log(res.data.totalPages);
